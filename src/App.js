9068ab5e02c5ca3d110eaade6c1ea588245d0323
jeisson <carrillojeisson2@gmail.com>
2023-03-25 11:40:35 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout } from "antd";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// pages
+import Home from "./pages/home";
+import Error404 from "./pages/error404";
+import Movie from "./pages/movie";
+import Serie from "./pages/serie";
+
+import NewMovies from "./pages/new-movies";
+import NewSeries from "./pages/new-series";
+
+import Popular from "./pages/popular";
+import Search from "./pages/search";
+
+import MenuTop from "./components/MenuTop";
+
+
 
 function App() {
+  const { Header, Content } = Layout;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Router>
+        <Header style={{zIndex: 1}}>
+          {/* zindex para que el menu sea clicleable despues de oscurecer el fondo */}
+          <MenuTop/>
+        </Header>
+
+        <Content>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Home />
+            </Route>
+
+          <Route path="/new-movies" exact={true}>
+            <NewMovies />
+          </Route>
+
+          <Route path="/popular" exact={true}>
+            <Popular />
+          </Route>
+
+          <Route path="/search" exact={true}>
+              <Search />
+            </Route>
+
+            <Route path="/movie/:id" exact={true}>
+              <Movie />
+            </Route>
+
+            <Route path="/serie/:id" exact={true}>
+              <Serie />
+            </Route>
+
+            <Route path="/new-series" exact={true}>
+              <NewSeries />
+            </Route>
+
+            <Route path="*" >
+              <Error404 />
+            </Route>
+          </Switch>
+
+        </Content>
+      </Router>
+    </Layout>
   );
 }
 
